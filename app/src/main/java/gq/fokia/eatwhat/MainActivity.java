02a36b2fef69c6390c4e982981548e9088feb03d
fokia <fokia@qq.com>
2017-03-30 13:49:32 +0800

    set navigationItem icon color

--- app/src/main/java/gq/fokia/eatwhat/MainActivity.java
+++ app/src/main/java/gq/fokia/eatwhat/MainActivity.java
@@ -62,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        //设置navigationItem图标按照自己本来颜色显示
+        navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -73,8 +75,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
     public void transparentState(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//5.0 全透明实现
-//getWindow.setStatusBarColor(Color.TRANSPARENT)
+            //5.0 全透明实现
+            //getWindow.setStatusBarColor(Color.TRANSPARENT)
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
