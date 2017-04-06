8007ff486a0dafb79f05a58e5f70e306632ec22f
fokia <fokia@qq.com>
2017-04-06 13:18:26 +0800

    add undo method at recentfood

--- app/src/main/java/gq/fokia/eatwhat/RecentCallBack.java
+++ app/src/main/java/gq/fokia/eatwhat/RecentCallBack.java
@@ -1,8 +1,10 @@
 package gq.fokia.eatwhat;
 
 import android.content.ContentValues;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
 import static gq.fokia.eatwhat.AllFoodFragment.foodList;
 import static gq.fokia.eatwhat.AllFoodFragment.recyclerView;
@@ -18,11 +20,33 @@ public class RecentCallBack extends MyCallBack {
         Log.d("RecentCallBack", getClass().toString());
         int position = viewHolder.getAdapterPosition();
         //此food的recent属性清零，将数据写入数据库，实现持久化
+        Food food = foodList.get(position);
         ContentValues contentValues = new ContentValues();
         contentValues.put("recent", 0);
         String[] args = {foodList.get(position).getName()};
         db.update("food", contentValues, "name=?", args);
         foodList.remove(position);
         recyclerView.getAdapter().notifyItemRemoved(position);
+        showSnackBar(viewHolder, "已从最近列表移除", food, position);
+    }
+    //TODO 交换顺序
+
+    @Override
+    public void showSnackBar(RecyclerView.ViewHolder view, String sentence, Food food, int position) {
+        current_food = food;
+        current_position = position;
+        Snackbar snackbar = Snackbar.make(view.itemView, sentence, Snackbar.LENGTH_SHORT);
+        snackbar.setAction("撤销", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("recent", System.currentTimeMillis());
+                String args[] = {current_food.getName()};
+                db.update("food", contentValues, "name=?", args);
+                foodList.add(current_position, current_food);
+                recyclerView.getAdapter().notifyDataSetChanged();
+            }
+        });
+        snackbar.show();
     }
 }
